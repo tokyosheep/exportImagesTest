245867ad98eed5429153a6cf7b0f0930fcfdc44a
Shuji <tokyosheep@hotmail.com>
2023-05-11 23:06:55 +0900

    update

--- src/fileSystem/root.ts
+++ src/fileSystem/root.ts
@@ -1,8 +1,9 @@
 import os from 'os';
 
+// you can get Desktop path below both Win and Mac.
 export const DESKTOPPATH:string = path.join(os.homedir(), 'Desktop');
 
-//file protocol. you need this. using on older version than Photoshop 24.4 
+//file protocol. you need this for using on older than Photoshop 24.4 
 const PROTOCOL = 'file:';
 
 export const setProtocol:(filePath:string)=>string = filePath => HASPROTOCOL ? path.join(PROTOCOL,filePath) : filePath ;
